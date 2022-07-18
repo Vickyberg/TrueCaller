0170b1cac1e6b5a3e214a79fee2228292ae4dd39
Victor Olamide <akinnusivictor098@gmail.com>
2022-07-18 17:13:35 +0100

    true Caller

--- src/africa/semicolon/trueCaller/data/models/Contact.java
+++ src/africa/semicolon/trueCaller/data/models/Contact.java
@@ -1,2 +1,50 @@
-package africa.semicolon.trueCaller.data.models;public class Contact {
+package africa.semicolon.trueCaller.data.models;
+
+public class Contact {
+    private int id; //identifier
+
+    private String firstName;
+    private String secondName;
+    private String phoneNumber;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
